102b0f8f83e97ea42b926968724756f70780f0fc
Siddharth Vekariya <siddharthvekariya510@gmail.com>
2023-11-23 12:18:42 +0530

    1

---
+++ Project Tesla/script.js
@@ -0,0 +1,28 @@
+// menu button for small screens
+function menu() {
+    const header = document.querySelector("header");
+    const section = document.querySelectorAll("#s1")
+    header.classList.toggle("active");
+
+    // if (header.classList.contains("active")==true) {
+    //     section.style.display = "none";
+    // } else {
+    //     section.style.display = "flex";
+    // }
+}
+
+
+// function vehicle() {
+//     // let vehicles = document.querySelectorAll(".vehicles")
+//     let drop = document.getElementById("dd")
+//     let s1 = document.querySelectorAll(".order_demo")
+
+//     if (drop.style.opacity == 0) {
+//         // drop.style.transform = "translateY(5%)"
+//         drop.style.opacity = "1"
+//     } else {
+//         // drop.style.transform = "translateY(0%)"
+//         drop.style.opacity = "0"
+//     }
+// }
+
